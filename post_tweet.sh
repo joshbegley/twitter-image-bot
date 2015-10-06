4ddec380ee7e54cf19cdb6f266b26eb1b057c931
Josh Begley <josh@joshbegley.com>
2015-10-06 12:32:35 -0400

    modified @everyword bot to tweet images

---
+++ post_tweet.sh
@@ -0,0 +1,3 @@
+python everywordbot.py --consumer_key=<ckey> --consumer_secret=<csecret> \
+      --access_token=<atoken> --token_secret=<tokensecret> \
+      --source_file=tweet_list.txt --index_file=current_line.txt
